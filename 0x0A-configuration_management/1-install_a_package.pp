4cf7a14dec1c369353ac0b7a9927e4d43e581571
Henzz <khenok53@gmail.com>
2024-03-28 01:55:45 +0000

    fixed lint

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -7,7 +7,7 @@ package { 'python3-pip':
 
 # Execute command to install Flask with a specific version using pip3
 exec { 'install_flask':
-  command  => '/usr/bin/pip3 install flask==2.1.0',
-  creates  => '/usr/local/lib/python3.9/dist-packages/flask',
-  require  => Package['python3-pip'],
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  creates => '/usr/local/lib/python3.9/dist-packages/flask',
+  require => Package['python3-pip'],
 }
