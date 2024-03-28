131255ea1b0dd5a9c16b93413784f131e6f5724a
Henzz <khenok53@gmail.com>
2024-03-28 01:49:45 +0000

    fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,12 @@
 # Using Puppet installs a flask from pip3 with specific version
-package { 'flask':
+
+# Resource class for managing pip packages
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Resource to install flask with a specific version
+package { 'python3-flask':
   ensure   => '2.1.0',
   provider => 'pip3',
   require  => Package['python3-pip'],
