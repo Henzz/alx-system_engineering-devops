a608251e8e008fb9a87191763e8a666b4cb98066
Henzz <khenok53@gmail.com>
2024-04-24 21:51:04 +0300

    task 1 fix python api

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -5,7 +5,7 @@ A script that exports data in the CSV format.
 import csv
 import json
 import sys
-import urllib.request as request
+import requests
 
 
 def export_to_csv(user_id):
@@ -18,20 +18,21 @@ def export_to_csv(user_id):
     user_url = f"{base_url}/users/{user_id}"
     user = {}
     user_id = int(user_id)
-    with request.urlopen(user_url) as response:
-        data = json.loads(response.read().decode('utf-8'))
+    with requests.get(user_url) as response:
+        data = response.json()
         user = data
 
-    with open(f"{user_id}.csv", 'w', newline='') as file:
-        writer = csv.writer(file)
-        with request.urlopen(todos_url) as response:
-            data = json.loads(response.read().decode('utf-8'))
+    with open(f"{user_id}.csv", 'w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        with requests.get(todos_url) as response:
+            data = response.json()
+
             for info in data:
                 if info.get('userId') == user_id:
                     writer.writerow([
-                        str(user_id),
-                        user.get('username'),
-                        info.get('completed'),
+                        str(user.get('id')),
+                        str(user.get('username')),
+                        str(info.get('completed')),
                         info.get('title')
                         ])
 
