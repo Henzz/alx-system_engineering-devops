c0302dc4f6d6ffaa63045dd6086fd486b0fc184f
Henzz <khenok53@gmail.com>
2024-05-16 21:46:57 +0300

    fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,18 +1,15 @@
 # This Puppet manifest updates the Nginx configuration file and sets ULIMIT="-n 4096"
 
-# Ensure file
-file { '/etc/default/nginx':
-  ensure  => file,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-}
-
 # Add ULIMIT="-n 4096" if it does not exist
 exec { 'add_ulimit_to_nginx':
   command => 'echo "ULIMIT=\"-n 4096\"" >> /etc/default/nginx',
   unless  => 'grep -q "^ULIMIT=\"-n 4096\"$" /etc/default/nginx',
   path    => ['/bin', '/usr/bin'],
+}
+
+# Ensure file
+file { '/etc/default/nginx':
+  ensure  => file,
 }
 
 # Ensure the Nginx service is running and enabled
